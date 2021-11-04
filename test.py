9702a146c84e1e4dccddfe142481e3ace7b2cf96
luigi-ravioli <luis_valera@dlsu.edu.ph>
2021-11-04 13:24:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 
-timestamp_option = b'\x44\x04\x05\x50\x00\x00\x00\x00\x44\x04\x05\x60\x00\x00\x00\x00\x44\x04\x05\x70\x00\x00\x00\x00\x44\x04\x05\x80\x00\x00\x00\x00\x44\x04\x05\x90'
-pkt_with_opts=IP(dst='10.1.0.1', options=IPOption(timestamp_option)) / UDP(sport=5792, dport=80)
+timestamp_option = bytes([b'\x44\x04\x05\x50', b'\x44\x04\x05\x60', b'\x44\x04\x05\x70', b'\x44\x04\x05\x80', b'\x44\x04\x05\x90'])
+pkt_with_opts=IP(dst='10.1.0.1', options=IPOption((timestamp_option)) / UDP(sport=5792, dport=80))
 pkt_with_opts.show2()
 
 pkt_without_opts=IP(dst='10.1.0.1') / UDP(sport=5792, dport=80)
@@ -10,3 +10,5 @@ hexdump = hexdump(pkt_with_opts)
 
 print(len(pkt_without_opts[IP].options))
 print(len(pkt_with_opts[IP].options))
+
+print(pkt_with_opts[IP].options)
