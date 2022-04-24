41b28960dfb935fd77a9dac235c358101e367723
luigi-ravioli <luis_valera@dlsu.edu.ph>
2022-04-25 07:50:40 +0800

    data collection

---
+++ data_collection.py
@@ -0,0 +1,25 @@
+import psutil
+import xlwt
+from xlwt import Workbook
+
+def collect_data(interval):
+    wb = Workbook()
+    sheet1 = wb.add_sheet('CPU USAGE')
+    row = 1
+    time = 0
+    sheet1.write(0, 0, "Seconds")
+    sheet1.write(0, 1, "CPU Usage")
+    print("Data collection started")
+    try:
+        while True:
+            sheet1.write(row, 0, time)
+            sheet1.write(row, 1, psutil.cpu_percent(interval))
+            row += 1
+            time += interval
+    
+    except KeyboardInterrupt:
+        input("Data collection stopped, press any key to continue...")
+        wb.save("data.xls")
+
+interval = input("Select interval in seconds for collecting CPU usage: ")
+collect_data(float(interval))
